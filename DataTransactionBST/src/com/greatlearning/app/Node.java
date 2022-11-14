da819d5f7514a48e0be7a9dcb7a30d40a427625e
Mirza-Narjis <110598038+Mirza-Narjis@users.noreply.github.com>
2022-11-14 13:34:59 +0530

    Add files via upload

---
+++ DataTransactionBST/src/com/greatlearning/app/Node.java
@@ -0,0 +1,15 @@
+package com.greatlearning.app;
+
+public class Node {
+	public int data;
+	public Node left;
+	public Node right;
+	
+	//constructor
+	public Node(int num)
+	{
+		data = num;
+		left = right = null;
+	}
+
+}
